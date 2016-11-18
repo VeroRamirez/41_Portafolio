2f07f0d71dd9b3eaee149761f445a1beb5c8ef6e
VeroRamirez <veronica12.rp@gmail.com>
2016-11-18 16:49:59 -0600

    Version 2 portafolio

--- js/main.js
+++ js/main.js
@@ -8,8 +8,27 @@ $(document).ready(function(){
           slidesToScroll: 1,
           autoplay: true,
           autoplaySpeed: 2000,
-    });
-    
-    
+    });		
 });
 
+$( function() {
+
+    $( '#cd-dropdown1' ).dropdown( {
+        gutter : 5,
+        delay : 40,
+        rotated : 'left'
+    } );
+    
+    $( '#cd-dropdown2' ).dropdown( {
+        gutter : 5,
+        delay : 40,
+        rotated : 'left'
+    } );
+    
+    $( '#cd-dropdown3' ).dropdown( {
+        gutter : 5,
+        delay : 40,
+        rotated : 'left'
+    } );
+
+});
