9e6d44fa2ecef73511eef4a03f45d924f3c31189
VeroRamirez <veronica12.rp@gmail.com>
2016-12-01 17:39:44 -0600

    hola

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
     $('#idFoto').hover(function(){
         $('#idCapaImg').removeClass('capa-img');
-    },function(){$('#idCapaImg').addClass('capa-img');});		
+    },function(){$('#idCapaImg').addClass('capa-img');});	
+    
+    
+    //Muestra y esconde menu
+	$(".opMenu").on("click", function(){
+		// Si esta en pequeno lo oculta
+	 	if ( $(window).width() <= 768 ) {
+	 		$("#myNavbar").hide(100);
+	 	}
+	});
+	$("#homeNav").on("click", function(){
+	 	$("#myNavbar").toggle(100)
+	});
+    
 });
 
 $( function() {
@@ -26,6 +39,11 @@ $( function() {
 
 });
 
+//Aumenta Fondo en la lista de habilidades
 function aumentaFondo(){
     $(this).addClass('aumentaFondo');
 } 
+
+
+
+
